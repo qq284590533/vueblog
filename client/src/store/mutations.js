104e41e22ea58cf51eab3ea377cf3eaa91521f0e
chenlin <284590533@qq.com>
2018-07-17 09:21:25 +0800

    格式化

--- client/src/store/mutations.js
+++ client/src/store/mutations.js
@@ -1,15 +1,15 @@
 const mutations = {
-	  	changeLogin(state){
-		    state.isLogin = !Boolean(Number(sessionStorage.getItem('isLogin')));
-			sessionStorage.setItem('isLogin',state.isLogin?'1':'0');
-		},
-		changeNight(state){
-			state.night = !state.night;
-		},
-		fullScreen(state){
-			state.fullScreen = !state.fullScreen;
-		}
-    
+	changeLogin(state) {
+		state.isLogin = !Boolean(Number(sessionStorage.getItem('isLogin')));
+		sessionStorage.setItem('isLogin', state.isLogin ? '1' : '0');
+	},
+	changeNight(state) {
+		state.night = !state.night;
+	},
+	fullScreen(state) {
+		state.fullScreen = !state.fullScreen;
+	}
+
 }
 
 
